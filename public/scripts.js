dfd51add4ffea437750a99c0715897bb11800b1a
William Silva <tintilho@yahoo.com.br>
2021-08-01 19:01:21 +0100

    Pequenas alterações.

---
+++ public/scripts.js
@@ -0,0 +1,74 @@
+const Modal = {
+  open() {
+    //Abrir o modal
+    //Adicionar a clall active ao modal
+    document.querySelector('.modal-overlay').classList.add('active')
+  },
+  close() {
+    //fechar o modal
+    //Remover a class active do modal
+    document.querySelector('.modal-overlay').classList.remove('active')
+  }
+}
+
+const Transactions = [
+  {
+    id: 1,
+    description: 'luz',
+    amount: -50000,
+    date: '23/01/2021'
+  },
+  {
+    id: 1,
+    description: 'website',
+    amount: -500000,
+    date: '23/01/2021'
+  },
+  {
+    id: 3,
+    description: 'internet',
+    amount: -20000,
+    date: '23/01/2021'
+  }
+]
+
+//eu preciso somar as entradas
+//depois eu preciso somar as saidas e
+//remover das entradas o valor das saidas
+//assim, eu erei o tatal
+
+const Transaction = {
+  incomes() {
+    //somar as entradas
+  },
+  expenses() {
+    //somar as saidas
+  },
+  total() {
+    //entradas - saidas
+  }
+}
+
+const inputFile = window.document.getElementById('input-image')
+let pickImage = window.document.getElementById('image-change')
+
+inputFile.addEventListener('change', event => {
+  function toDataUrl(url, callback) {
+    var xhr = new XMLHttpRequest()
+    xhr.onload = function () {
+      var reader = new FileReader()
+      reader.onloadend = function () {
+        callback(reader.result)
+      }
+      reader.readAsDataURL(xhr.response)
+    }
+    xhr.open('GET', url)
+    xhr.responseType = 'blob'
+    xhr.send()
+  }
+
+  pickImage
+  console.log({
+    event: event.target.value
+  })
+})
